76b88f3f736d41e0ec0c79fe33e74d49dc41b2c8
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-08 17:40:12 +0530

    Implemented UC2: Ability to add new contact from console

--- src/main/java/com/addressbooksystem/AddressBookMain.java
+++ src/main/java/com/addressbooksystem/AddressBookMain.java
@@ -1,13 +1,36 @@
 package com.addressbooksystem;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
 
         AddressBook addressBook = new AddressBook();
 
-        addressBook.addContact("Vaibhav", "Raut", "ABC Main St", "Pune", "MH", "444404", "9579354301", "vaibhav.raut851@gmail.com");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Enter Last Name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Enter Address: ");
+        String address = scanner.nextLine();
+        System.out.print("Enter City: ");
+        String city = scanner.nextLine();
+        System.out.print("Enter State: ");
+        String state = scanner.nextLine();
+        System.out.print("Enter Zip: ");
+        String zip = scanner.nextLine();
+        System.out.print("Enter Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.print("Enter Email: ");
+        String email = scanner.nextLine();
+
+        addressBook.addContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
 
         addressBook.displayContacts();
+
+        scanner.close();
     }
 }
